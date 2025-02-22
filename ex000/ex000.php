14c59c89eabc0def906028c962503b323c97269f
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2025-02-22 19:05:42 +0000

    feat:  Variables Declaration

--- ex000/ex000.php
+++ ex000/ex000.php
@@ -1,4 +1,13 @@
-<php
+<html>
+<head>
+    <title>ex000</title>
+</head>
+<body>
+    <h1>ex000</h1>
+    <?php
 phpinfo();
-
 ?>
+</body>
+</html>
+
+
---
+++ ex002/ex002.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ex002</title>
+</head>
+<body>
+
+
+<?php 
+
+date_default_timezone_set('America/Sao_Paulo');
+echo "Hoje é dia " . date("d/m/Y") . "<br>";
+?>
+    
+</body>
+</html>
