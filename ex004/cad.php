bef07bd0c42ada4046076088d1427766f1ecc948
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2025-02-23 21:02:07 +0000

    chore:  PHP form

--- ex004/cad.php
+++ ex004/cad.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Resultado</title>
 </head>
 <body>
+    <header>
+        <h1>Resultado do formulario</h1>
+    </header>
+
+    <main>
+        <?php
+            $nome = $_POST['nome'];
+            $sobrenome = $_POST['sobrenome'];
+            echo "Olá $nome $sobrenome! Seja bem vindo!";
+        ?>
+    </main>
     
 </body>
 </html>
