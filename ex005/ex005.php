238591492c5659b67f3550580abaa66b6400fcf1
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2025-02-23 23:04:08 +0000

    chore:  PHP desafios

--- ex005/ex005.php
+++ ex005/ex005.php
@@ -20,12 +20,30 @@
      function ConversorDeMoedasDeKwanzaParaEuro(float $valor):string{
          $conversor = $valor / 1180;
          $valorConvertido = number_format($conversor,2,".","");
+         
 
          return " $valor kwanza convertido para euro é $valorConvertido euros";
 
      }
 
      echo ConversorDeMoedasDeKwanzaParaEuro(100000);
+
+     function gerarNumeroAleatorio():int{
+         //return rand(0,100);
+         //return random_int(0,100);
+         return mt_rand(0,100);
+     }
+
+     echo gerarNumeroAleatorio();
+
+
+     function DividirUmNumeroComVirgulaParteInteiraParteDecimal(float $valor):string{
+         $parteInteira = (int)$valor;
+         $parteDecimal = $valor - $parteInteira;
+         return "A parte inteira do $valor é $parteInteira e a parte decimal é $parteDecimal";
+     }
+
+     echo DividirUmNumeroComVirgulaParteInteiraParteDecimal(5.54);
 
     ?>
 </body>
