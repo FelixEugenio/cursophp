c4db1861e825ff6bcf0ffcdc69f0ec3602351fc7
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2025-02-24 20:09:15 +0000

    feat: superglobais

---
+++ ex006/ex006.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Superglobais</title>
+</head>
+<body>
+    <?php 
+           var_dump($_SERVER); 
+           var_dump($_GET);
+           var_dump($_POST);
+           var_dump($_COOKIE);
+           var_dump($_FILES);
+           var_dump($_COOKIE);
+           var_dump($_FILES);
+           var_dump($_COOKIE);
+           
+    ?>
+</body>
+</html>
