ee71dc26787c7bba1d55943522cf6cf0815b7193
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2025-02-25 20:20:05 +0000

    feat: Metodos Magicos

---
+++ ex019/ex019.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Metodos Magicos</title>
+</head>
+<body>
+    <?php 
+
+    //exemplo com get e set
+    class Pessoa {
+
+    private  $dados = [];
+
+    public function __set(string $name,string $value) {
+      $this->dados[$name] = $value;
+    }
+
+    public function __get(string $name) {
+      return $this->dados[$name] ?? null;
+    }
+}
+
+$pessoa = new Pessoa();
+$pessoa->nome = "Felix";
+echo $pessoa->nome;
+    
+    ?>
+    
+</body>
+</html>
